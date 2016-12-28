5a48f07b4c0fd0fc6047b070c7084e72be1f5a9c
Dillon Christensen <j.dillon.christensen+git@gmail.com>
2016-12-28 16:40:48 -0600

    added some sugary sweetness to the slack responder

--- response-transformer.js
+++ response-transformer.js
@@ -8,12 +8,29 @@ module.exports = function transformApiResponse(document) {
         }
     }
 
+    function getItemPrice(el) {
+        let itemPrice = parseFloat(el.querySelector('.item-price .price').innerHTML.substr(4));
+        let shipping = el.querySelector('.shipping .price');
+        if (shipping) {
+            itemPrice += parseFloat(shipping.textContent.substr(4));
+        }
+        return itemPrice;
+    }
+
+    function getItemImage(el) {
+        let img = el.querySelector('.result-image img');
+        if(el.querySelector('.no-book-image') || !img) {
+            return null;
+        }
+        return img.src;
+    }
+
     return Array.from(searchResults)
-        .slice(0, 3)
         .map(el => ({
                 year: getItemProp('datePublished', el),
                 about: getItemProp('about', el),
-                price: el.querySelector('.item-price .price').innerHTML.substr(4)
+                price: getItemPrice(el),
+                image: getItemImage(el)
             })
         );
 };
--- slack-responder.js
+++ slack-responder.js
@@ -1,37 +1,63 @@
 module.exports = slacker = (query, searchUrl, x) => {
-    let demo = {
-        "attachments": [
-            {
-                "fallback": "Required plain-text summary of the attachment.",
-                "color": "#36a64f",
-                "author_name": "Price Results",
-                "title": "Capote - In Cold Blood - Random - 1965",
-                "title_link": "https://api.slack.com/",
-                "fields": [
-                    {
-                        "title": "$23.99",
-                        "short": false
-                    },
-                    {
-                        "title": "$45.99",
-                        "short": false
-                    }
-                ]
-            }
-        ]
-    };
+    const MAX_RESULTS = 50;
+    const RESULTS_LIMIT = 5;
+    /**
+     * slack doesn't need full results listed, just first three
+     * @type {*}
+     */
+    let sharedResults = x.slice(0, RESULTS_LIMIT);
+    /**
+     * search title for quick reference in searches of old queries
+     * @type {string}
+     */
     let searchTitle = Object.keys(query)
         .map(k => query[k])
         .join(' - ');
+    /**
+     * counting unshown results here
+     * @type {string}
+     */
+    let hiddenResultsIdenifier = x.length === MAX_RESULTS ? 'many' : x.length - RESULTS_LIMIT > 0 ? 'some' : 'no';
+    /**
+     * color-coding the message based on result count
+     * @type {string}
+     */
+    let hiddenResultsColor = hiddenResultsIdenifier === 'many' ?
+        '01897B' : hiddenResultsIdenifier === 'some' ? 'FDD838' : 'EF5A53';
+    /**
+     * building response in var for debugging
+     * FIXME: just return stringified response after tested fully
+     * @type {{attachments: [*]}}
+     */
     let response = {
-      attachments: [
-          {
-              author_name: "Bookmancy Price Results",
-              title: searchTitle,
-              title_link: searchUrl,
-              fields: x.map(r => ({short: false, title: r.price}))
-          }
-      ]
+        attachments: [
+            {
+                author_name: "Bookmancy Price Results",
+                color: `#${hiddenResultsColor}`,
+                pretext: `Searched for \`${searchTitle}\``,
+                text: `There are ${hiddenResultsIdenifier} more results in the search above :point_up:`,
+                title: `See Search Results For: ${searchTitle}`,
+                title_link: searchUrl,
+                fields: sharedResults.map(r => ({
+                    short: false,
+                    title: r.price + (r.year ? ` (${r.year})`: ''),
+                    value: r.about.length > 120 ?
+                        r.about.substr(0, 120) + '...' :
+                        r.about
+                })),
+                fallback: searchTitle + sharedResults.map(r => r.price).join(' :: '),
+                footer: 'AbeBooks API',
+                footer_icon: 'https://www.abebooks.com/images/gateway/heroes/bookbird-avatar.png',
+                ts: Math.floor(new Date().getTime() / 1000)
+            }
+        ]
     };
+    let resultWithImage = x.find(r => !!r.image);
+    if (resultWithImage) {
+        response.attachments[0].thumb_url = resultWithImage.image;
+    }
+    /**
+     * this is directly fed to the user's browser
+     */
     return JSON.stringify(response);
 };
