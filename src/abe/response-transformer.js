89a1a87066f143cee989d030e41935c78142607e
Dillon Christensen <j.dillon.christensen@gmail.com>
2018-08-10 21:55:18 -0500

    updates to abe class names

--- src/abe/response-transformer.js
+++ src/abe/response-transformer.js
@@ -1,7 +1,9 @@
 const toResult = require('../result-model.js');
 
 const getItemProp = (name, elem) => {
-    const metaTag = elem.querySelectorAll('meta').find(m => m.attributes.itemprop && m.attributes.itemprop === name);
+    const metaTag = elem.querySelectorAll('meta')
+        .find(m => m.attributes.itemprop && m.attributes.itemprop === name);
+
     if (metaTag) {
         return metaTag.attributes.content;
     }
@@ -10,10 +12,10 @@ const getItemProp = (name, elem) => {
 const getUrl = el => {
     const a = el.querySelector('[itemprop="url"]');
     return a && `https://www.abebooks.com${a.attributes.href}`;
-}
+};
 
 const getItemPrice = el => {
-    const price = el.querySelector('.item-price .price');
+    const price = el.querySelector('.srp-item-price') || el.querySelector('.price');
     if (price) {
         return price.innerHTML.substr(4);
     }
@@ -21,7 +23,9 @@ const getItemPrice = el => {
 };
 
 const getItemShipping = el => {
-    const shipping = el.querySelector('.shipping .price');
+    const shipping = el.querySelector('.srp-item-price-shipping') ||
+        el.querySelector('.shipping .price');
+
     if (shipping) {
         return shipping.text.substr(4);
     }
@@ -38,12 +42,13 @@ const getItemImage = el => {
 
 module.exports = (document) => {
     return document.querySelectorAll('.cf.result')
-        .map(el => toResult({
-                year: getItemProp('datePublished', el),
-                about: getItemProp('about', el),
-                price: getItemPrice(el),
-                shipping: getItemShipping(el),
-                image: getItemImage(el),
-                url: getUrl(el)
-            }));
+        .map((el) => toResult({
+            year: getItemProp('datePublished', el),
+            about: getItemProp('about', el),
+            price: getItemPrice(el),
+            shipping: getItemShipping(el),
+            image: getItemImage(el),
+            url: getUrl(el)
+        }));
 };
+
